cd58c2804217689e72960623c197fc8fac0e0602
rahulprasad0710 <raulshah2021@hotmail.com>
2023-08-09 08:07:02 +0545

    -add:className file system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
     return (
         <div className='App'>
-            <h1>Hello WOrld</h1>
+            {/* <h1
+                style={{
+                    color: "red",
+                    padding: "20px",
+                    backgroundColor: "black",
+                    paddingBottom: "20px",
+                }}>
+                Hello WOrld
+            </h1>
+            <h2 className='heading'>hello World 2</h2>
+            <h2 className='heading'>hello World 3</h2>
+            <h2 className='heading'>hello World 4</h2>
+            <Header /> */}
+
+            <h3 className='heading'>App heading</h3>
+            <Header />
         </div>
     );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+    return (
+        <div className='navbar'>
+            <div>
+                <h3 className='heading'>Navbar</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
